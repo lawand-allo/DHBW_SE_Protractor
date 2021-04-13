e07232a9b4cb765c43b0ca638264ea915648f8fc
lawand.allo <lawand.allo@sap.com>
2021-04-13 11:35:43 +0200

    write some e2e tests

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,16 +1,20 @@
-import { browser, logging } from 'protractor';
+import {browser, logging} from 'protractor';
 import { AppPage } from './app.po';
 
 describe('workspace-project App', () => {
-  let page: AppPage;
-
-  beforeEach(() => {
-    page = new AppPage();
-  });
+  const page: AppPage = new AppPage();
 
   it('should display welcome message', async () => {
     await page.navigateTo();
-    expect(await page.getTitleText()).toEqual('toDoApp app is running!');
+    expect(await page.getTitleText()).toEqual('ToDo List');
+  });
+
+  it('should find first todo', async () => {
+    expect(await page.getToDoName('todo0')).toEqual('Einkaufen');
+  });
+  it('should add a new Todo', async () => {
+    await page.addTodo('SE Blog Entry');
+    expect(await page.getToDoName('todo2')).toEqual('SE Blog Entry');
   });
 
   afterEach(async () => {
--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -6,6 +6,18 @@ export class AppPage {
   }
 
   async getTitleText(): Promise<string> {
-    return element(by.css('app-root .content span')).getText();
+    return element(by.className('header')).getText();
+  }
+
+  async getToDoName(todoId: string): Promise<string> {
+    return element(by.id(todoId)).getText();
+  }
+
+  async addTodo(todoName: string): Promise<void> {
+    await browser.sleep(3000);
+    await element(by.id('nameField')).sendKeys(todoName);
+    await browser.sleep(3000);
+    await element(by.className('todo-submit')).click();
+    await browser.sleep(3000);
   }
 }
